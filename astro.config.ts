0f776a38211e08e84b415d332e6478e9c9035871
derecskey <david.derecskey@gmail.com>
2026-03-31 18:51:14 -0400

    fix: add trailing slash to base URL in Astro configuration

--- astro.config.ts
+++ astro.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: 'https://derecskey.github.io',
-  base: '/valleypaintingco',
+  base: '/valleypaintingco/',
 });
