b6f21d1e6fc6450138aa21cee5e77c5f81597bf4
derecskey <david.derecskey@gmail.com>
2026-03-31 18:44:32 -0400

    fix: update site URL in Astro configuration

--- astro.config.ts
+++ astro.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://valleypaintingco.com',
+  site: 'https://derecskey.github.io',
+  base: '/valleypaintingco',
 });
